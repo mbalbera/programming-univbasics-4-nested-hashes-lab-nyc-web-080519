4b3867f0961b42d00f2104d8ce844ed9160e90b4
Mason Balbera <mhbalbera@gmail.com>
2019-07-29 15:46:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -5,9 +5,18 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        }}
-  
-	programmer_hash[:grace_hopper]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+
 end
 
 def alan_kay_is_known_for
